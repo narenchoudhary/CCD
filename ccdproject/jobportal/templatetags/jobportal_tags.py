c8ca4462df4448f695141e646066185a39dbea46
Narendra Choudhary <n.choudhary@iitg.ac.in>
2016-09-12 16:48:50 +0530

    Rework templatetags

--- ccdproject/jobportal/templatetags/jobportal_tags.py
+++ ccdproject/jobportal/templatetags/jobportal_tags.py
@@ -1,43 +1,35 @@
 from django import template
-from django.utils import timezone
+from django.utils import timezone, dateparse
 
 from jobportal.models import SiteManagement
 
 register = template.Library()
 
 
-@register.filter(expects_localtime=True, is_safe=True)
+@register.simple_tag
 def date_passed(value):
-    try:
-        print value
-        return value < timezone.now().date()
-    except ValueError:
+    """
+    Returns True if deadline has passed
+    :param value: Query sting for which deadline status is needed
+    :return: True/False
+    """
+    site_management = SiteManagement.objects.all()[0]
+    now = timezone.now()
+    if value is 'job_stud_sign':
+        return site_management.job_student_sign_update_deadline < now
+    elif value == 'job_stud_avatar':
+        return site_management.job_student_avatar_update_deadline < now
+    elif value == 'job_stud_profile':
+        return site_management.job_student_profile_update_deadline < now
+    elif value == 'job_stud_cv':
+        return site_management.job_student_cv_update_deadline < now
+    elif value == 'intern_stud_sign':
+        return site_management.intern_student_sign_update_deadline < now
+    elif value == 'intern_stud_avatar':
+        return site_management.intern_student_avatar_update_deadline < now
+    elif value == 'intern_stud_cv':
+        return site_management.intern_student_cv_update_deadline < now
+    elif value == 'intern_stud_profile':
+        return site_management.intern_student_profile_update_deadline < now
+    else:
         return True
-
-
-@register.simple_tag(name='job_student_profile_update_deadline_passed')
-def job_student_profile_update_deadline():
-    site_management = SiteManagement.objects.all()[0]
-    now = timezone.now()
-    return site_management.job_student_profile_update_deadline < now
-
-
-@register.simple_tag(name='job_student_cv_update_deadline_passed')
-def job_student_cv_update_deadline():
-    site_management = SiteManagement.objects.all()[0]
-    now = timezone.now()
-    return site_management.job_student_cv_update_deadline < now
-
-
-@register.simple_tag(name='job_student_avatar_update_deadline_passed')
-def job_student_avatar_update_deadline():
-    site_management = SiteManagement.objects.all()[0]
-    now = timezone.now()
-    return site_management.job_student_avatar_update_deadline < now
-
-
-@register.simple_tag(name='job_student_sign_update_deadline_passed')
-def job_student_sign_update_deadline():
-    site_management = SiteManagement.objects.all()[0]
-    now = timezone.now()
-    return site_management.job_student_sign_update_deadline < now
