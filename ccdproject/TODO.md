8b79447571cf7fa6161bff3956a166339187d87e
Narendra Choudhary <n.choudhary@iitg.ac.in>
2016-09-14 21:56:03 +0530

    deleted ccdportal

--- ccdproject/TODO.md
+++
@@ -1,6 +0,0 @@
-#TODOs
-
-- [ ] Change complete front end design
-- [ ] Form Errors in MDL
-- [ ] Add guidelines for avatar and signature on student side
-- [ ] Find ways to avoid brute force
--- ccdproject/mentormentee/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from models import ResearchProposal, StudentProposalRelation
-
-admin.site.register(ResearchProposal)
-admin.site.register(StudentProposalRelation)
